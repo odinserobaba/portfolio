adb162aa1f14993e64ad7de749cb4a25667c7d80
odinserobaba <odinserobaba@gmail.com>
2023-03-19 14:28:05 +0300

    Return s

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,5 @@ from .models import Project
 # Create your views here.
 def home(request):
     projects = Project.objects.all()
-    return render(request,'portfolio/home.html',{'projects':projects})
+
+    return render(request,'portfolio/home.html',context={'projects':projects})
